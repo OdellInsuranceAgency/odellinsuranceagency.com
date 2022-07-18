e891d0ff6ae35e559dc91af3082f2ed2c8f7af79
RobertMOdell <44739719+RobertMOdell@users.noreply.github.com>
2022-07-17 23:50:10 -0700

    React control over phone and email (#14)

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -27,74 +27,74 @@ const Customers: NextPage = () => {
                         <div style={{float: "left", width: "450px"}}>
                             <a href="https://www.milwaukeecrane.com">
                                 <div className="logo">
-                                    <img src="/images/milwaukee crane.jpg" title="Milwaukee Crane"/>
+                                    <img src="/images/milwaukee crane.jpg" alt="Milwaukee Crane"/>
                                 </div>
                             </a>
                             <a href="https://www.tvtdiecast.com">
                                 <div className="logo">
-                                    <img src="/images/tvtdiecasting.jpg" title="TVT Die Casting"/>
+                                    <img src="/images/tvtdiecasting.jpg" alt="TVT Die Casting"/>
                                 </div>
                             </a>
                             {/*<a href="https://www.lambsmarkets.com">*/}
-                            {/*    <div className="logo"><img src="/images/lambs.jpg" title="Lambs Markets"/></div>*/}
+                            {/*    <div className="logo"><img src="/images/lambs.jpg" alt="Lambs Markets"/></div>*/}
                             {/*</a>*/}
                             <a href="https://www.rbslumber.com/">
                                 <div className="logo">
-                                    <img src="/images/rosenbergbuilders.jpg" title="Rosenberg Builders"/>
+                                    <img src="/images/rosenbergbuilders.jpg" alt="Rosenberg Builders"/>
                                 </div>
                             </a>
                             <a href="https://www.da-pdx.com">
                                 <div className="logo">
-                                    <img src="/images/dermatologyassociates.jpg" title="Dermatology Associates"/>
+                                    <img src="/images/dermatologyassociates.jpg" alt="Dermatology Associates"/>
                                 </div>
                             </a>
                             <a href="https://vanportdesign.com/">
                                 <div className="logo">
-                                    <img src="/images/vanportdesign.jpg" title="Van-Port Design"/>
+                                    <img src="/images/vanportdesign.jpg" alt="Van-Port Design"/>
                                 </div>
                             </a>
                         </div>
                         <div style={{float: "right", width: "450px"}}>
                             <a href="https://srcfab.com/">
                                 <div className="logo">
-                                    <img src="/images/src.jpg" title="Scientific Research Company"/>
+                                    <img src="/images/src.jpg" alt="Scientific Research Company"/>
                                 </div>
                             </a>
                             {/*<a href="https://www.pro-lineswa.com">*/}
-                            {/*    <div className="logo"><img src="/images/pro line.jpg" title="Pro-Lines"/></div>*/}
+                            {/*    <div className="logo"><img src="/images/pro line.jpg" alt="Pro-Lines"/></div>*/}
                             {/*</a>*/}
                             <a href="https://www.wereorganized.com/">
                                 <div className="logo">
-                                    <img src="/images/wereorganized.jpg" title="We're Organized"/>
+                                    <img src="/images/wereorganized.jpg" alt="We're Organized"/>
                                 </div>
                             </a>
                             <a href="https://www.crystalgreenslandscape.com">
                                 <div className="logo">
-                                    <img src="/images/crystalgreens.jpg" title="Crystal Greens Landscaping"/>
+                                    <img src="/images/crystalgreens.jpg" alt="Crystal Greens Landscaping"/>
                                 </div>
                             </a>
                             {/*<a href="https://tsgrayconstruction.com">*/}
                             {/*    <div className="logo"><img src="/images/tsgrayconstruction.jpg"*/}
-                            {/*                               title="TS Gray Construction"/></div>*/}
+                            {/*                               alt="TS Gray Construction"/></div>*/}
                             {/*</a>*/}
                             <a href="http://www.osmpi.com/home.html">
                                 <div className="logo">
-                                    <img src="/images/oregonscrew.jpg" title="Oregon Screw Machine Products Inc."/>
+                                    <img src="/images/oregonscrew.jpg" alt="Oregon Screw Machine Products Inc."/>
                                 </div>
                             </a>
                             <a href="http://www.precisionpowderinc.com">
                                 <div className="logo">
-                                    <img src="/images/precisionpowder.jpg" title="Precision Powder Coating, Inc."/>
+                                    <img src="/images/precisionpowder.jpg" alt="Precision Powder Coating, Inc."/>
                                 </div>
                             </a>
                         </div>
                         {/*Removed Items*/}
                         {/*<a href="http://www.internationalgarden.com"><div class="logo"><img src="/images/internationalgarden.jpg" alt="International Garden Products" /></div></a>*/}
-                        {/*<a href="http://www.iselinursery.com"><div class="logo"><img src="/images/iselinursery.jpg" title="Iseli Nursery" /></div></a>*/}
-                        {/*<a href="http://www.weeksroses.com"><div class="logo"><img src="/images/roses.jpg" title="Weeks Roses" /></div></a>*/}
-                        {/*<a href="http://www.rgpi.net"><div class="logo"><img src="/images/robertgraypartners.jpg" title="Robert Gray Partners" /></div></a>*/}
-                        {/*<a href="http://www.c2f.com"><div class="logo"><img src="/images/c2f.jpg" title="C2F" /></div></a>*/}
-                        {/*<a href="http://www.mici.com"><div class="logo"><img src="/images/maddenlogo.jpg" title="Madden Industrial Craftsmen" /></div></a>*/}
+                        {/*<a href="http://www.iselinursery.com"><div class="logo"><img src="/images/iselinursery.jpg" alt="Iseli Nursery" /></div></a>*/}
+                        {/*<a href="http://www.weeksroses.com"><div class="logo"><img src="/images/roses.jpg" alt="Weeks Roses" /></div></a>*/}
+                        {/*<a href="http://www.rgpi.net"><div class="logo"><img src="/images/robertgraypartners.jpg" alt="Robert Gray Partners" /></div></a>*/}
+                        {/*<a href="http://www.c2f.com"><div class="logo"><img src="/images/c2f.jpg" alt="C2F" /></div></a>*/}
+                        {/*<a href="http://www.mici.com"><div class="logo"><img src="/images/maddenlogo.jpg" alt="Madden Industrial Craftsmen" /></div></a>*/}
                     </div>
                 </div>
                 <div className="clearing">&nbsp;</div>
